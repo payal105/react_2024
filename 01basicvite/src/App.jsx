56e4cb1150bfd9951cbca31dc5e3f8e8344edc11
Payal <payaladhikary2000@gmail.com>
2024-07-22 07:52:10 +0530

    end of custom react

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,12 +1,12 @@
 import Youtube from "./Youtube"
 
 function App() {
-  
+  const username = "payal";
 
   return (
     <>
-      <h1>Vite React App</h1>
-      <h1>Vite React App</h1>
+      <h1>Vite React {" "} App {2+2}</h1>
+      <h1>Vite React App {username}</h1>
       <Youtube/>
     </>
   )
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,8 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+function MyApp() {
+    return(
+        <>
+            <h1>Custom React App</h1>
+        </>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'click to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <App/>
+    // AnotherElement
+    // areactElement
 )
