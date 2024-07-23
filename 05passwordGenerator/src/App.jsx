2a342a5111374c6257d2c98ee55107d187341310
unknown <payal.a@zicorp.in>
2024-07-23 12:10:47 +0530

    pswd generator part 2

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
+import { useState,useCallback,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //states
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password,setPassword] = useState('')
 
+  const generatePassword = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str+= "01234589"
+    if(charAllowed) str+= "!@#$%^&*()_+"
+
+    for (let i = 1; i < length; i++) {
+     
+      const char = Math.floor(Math.random() * str.length + 1)
+      pass+= str.charAt(char)
+    }
+    setPassword(pass)
+  },[length,numberAllowed,charAllowed])
+
+  useEffect(() => {
+    generatePassword()
+  },[length,numberAllowed,charAllowed])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+     <h1 className='text-white text-center my-3'>Password Generator</h1>
+     <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text" 
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='Password'
+        readOnly
+        />
+        <button
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+     </div>
+
+     <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e) => setLength(e.target.value)}
+           name=""
+            id="" />
+            <label htmlFor="length">Length: {length}</label>
+        </div>
+     </div>
+
+     <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" 
+          defaultChecked={numberAllowed}
+          onChange={(e)=>{
+            setNumberAllowed((prev) => !prev)
+          }}
+          name="" id="" 
+          />
+          <label htmlFor="number">Numbers</label>
+        </div>
+     </div>
+
+     <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" 
+          defaultChecked={charAllowed}
+          onChange={(e)=>{
+            setCharAllowed((prev) => !prev)
+          }}
+          name="" id="" 
+          />
+          <label htmlFor="charInput">Characters</label>
+        </div>
+     </div>
+    </div>
   )
 }
 
